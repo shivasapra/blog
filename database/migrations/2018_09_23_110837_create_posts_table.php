a6a1db67d3100298ca0e59b5c41a922a6c8ea47b
shiva <ashishsapra7@gmail.com>
2018-09-26 23:57:29 +0530

    Users table seeder added

--- database/migrations/2018_09_23_110837_create_posts_table.php
+++ database/migrations/2018_09_23_110837_create_posts_table.php
@@ -16,9 +16,11 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
+            $table->string('slug');
             $table->text('content');
             $table->string('featured');
             $table->integer('category_id');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
