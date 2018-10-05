6c0686fea634fb0c596938276fb9ae839948778c
shiva <ashishsapra7@gmail.com>
2018-10-05 12:49:35 +0530

    Dynamic content display 5

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -121,29 +121,31 @@
 	                        </div>
 	                    </div>
 	                </div>
-
+					
 	                <div class="pagination-arrow">
-
-	                    <a href="#" class="btn-prev-wrap">
-	                        <svg class="btn-prev">
-	                            <use xlink:href="#arrow-left"></use>
-	                        </svg>
-	                        <div class="btn-content">
-	                            <div class="btn-content-title">Next Post</div>
-	                            <p class="btn-content-subtitle">Claritas Est Etiam Processus</p>
-	                        </div>
-	                    </a>
-
-	                    <a href="#" class="btn-next-wrap">
-	                        <div class="btn-content">
-	                            <div class="btn-content-title">Previous Post</div>
-	                            <p class="btn-content-subtitle">Duis Autem Velius</p>
-	                        </div>
-	                        <svg class="btn-next">
-	                            <use xlink:href="#arrow-right"></use>
-	                        </svg>
-	                    </a>
-
+	                	@if($next)
+							<a href="{{route('post.single',['slug'=>$next->slug])}}" class="btn-next-wrap">
+		                        <div class="btn-content">
+		                            <div class="btn-content-title">Next Post</div>
+		                            <p class="btn-content-subtitle">{{$next->title}}</p>
+		                        </div>
+		                        <svg class="btn-next">
+		                            <use xlink:href="#arrow-right"></use>
+		                        </svg>
+		                    </a>
+						@endif
+						@if($prev)
+		                    <a href="{{route('post.single',['slug'=>$prev->slug])}}" 
+		                       class="btn-prev-wrap">
+		                        <svg class="btn-prev">
+		                            <use xlink:href="#arrow-left"></use>
+		                        </svg>
+		                        <div class="btn-content">
+		                            <div class="btn-content-title">Previous Post</div>
+		                            <p class="btn-content-subtitle">{{$prev->title}}</p>
+		                        </div>
+		                    </a>
+	                    @endif
 	                </div>
 
 	                <div class="comments">
